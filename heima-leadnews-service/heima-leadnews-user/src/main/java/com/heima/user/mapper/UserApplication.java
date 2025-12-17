0b797945ea66a8bfedd1a273ac677cd331edcc9e
MINGHUI NIE <107062041+NNNCODE@users.noreply.github.com>
2025-12-17 22:22:45 +0800

    1.Initial commit

---
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/mapper/UserApplication.java
@@ -0,0 +1,21 @@
+package com.heima.user.mapper;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Nmh
+ * @version 1.0
+ * @description TODO
+ * @date 12/16/2025
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.heima.user.mapper")
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
